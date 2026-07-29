c67a472020ecbb300f0e2fde98c3690a5df8b479
Rqirus <1078045338@qq.com>
2026-07-29 13:57:02 +0800

    feat: add a share datasource

--- apps/api/src/app/endpoints/data-providers/ghostfolio/ghostfolio.module.ts
+++ apps/api/src/app/endpoints/data-providers/ghostfolio/ghostfolio.module.ts
@@ -12,6 +12,7 @@ import { FinancialModelingPrepService } from '@ghostfolio/api/services/data-prov
 import { GoogleSheetsService } from '@ghostfolio/api/services/data-provider/google-sheets/google-sheets.service';
 import { ManualService } from '@ghostfolio/api/services/data-provider/manual/manual.service';
 import { RapidApiService } from '@ghostfolio/api/services/data-provider/rapid-api/rapid-api.service';
+import { StockApiService } from '@ghostfolio/api/services/data-provider/stock-api/stock-api.service';
 import { YahooFinanceService } from '@ghostfolio/api/services/data-provider/yahoo-finance/yahoo-finance.service';
 import { FetchModule } from '@ghostfolio/api/services/fetch/fetch.module';
 import { MarketDataModule } from '@ghostfolio/api/services/market-data/market-data.module';
@@ -48,6 +49,7 @@ import { GhostfolioService } from './ghostfolio.service';
     GoogleSheetsService,
     ManualService,
     RapidApiService,
+    StockApiService,
     YahooFinanceService,
     YahooFinanceDataEnhancerService,
     {
@@ -59,6 +61,7 @@ import { GhostfolioService } from './ghostfolio.service';
         GoogleSheetsService,
         ManualService,
         RapidApiService,
+        StockApiService,
         YahooFinanceService
       ],
       provide: 'DataProviderInterfaces',
@@ -70,6 +73,7 @@ import { GhostfolioService } from './ghostfolio.service';
         googleSheetsService,
         manualService,
         rapidApiService,
+        stockApiService,
         yahooFinanceService
       ) => [
         alphaVantageService,
@@ -79,6 +83,7 @@ import { GhostfolioService } from './ghostfolio.service';
         googleSheetsService,
         manualService,
         rapidApiService,
+        stockApiService,
         yahooFinanceService
       ]
     }
--- apps/api/src/services/data-provider/data-provider.module.ts
+++ apps/api/src/services/data-provider/data-provider.module.ts
@@ -9,6 +9,7 @@ import { GhostfolioService } from '@ghostfolio/api/services/data-provider/ghostf
 import { GoogleSheetsService } from '@ghostfolio/api/services/data-provider/google-sheets/google-sheets.service';
 import { ManualService } from '@ghostfolio/api/services/data-provider/manual/manual.service';
 import { RapidApiService } from '@ghostfolio/api/services/data-provider/rapid-api/rapid-api.service';
+import { StockApiService } from '@ghostfolio/api/services/data-provider/stock-api/stock-api.service';
 import { YahooFinanceService } from '@ghostfolio/api/services/data-provider/yahoo-finance/yahoo-finance.service';
 import { FetchModule } from '@ghostfolio/api/services/fetch/fetch.module';
 import { MarketDataModule } from '@ghostfolio/api/services/market-data/market-data.module';
@@ -44,6 +45,7 @@ import { DataProviderService } from './data-provider.service';
     GoogleSheetsService,
     ManualService,
     RapidApiService,
+    StockApiService,
     YahooFinanceService,
     {
       inject: [
@@ -55,6 +57,7 @@ import { DataProviderService } from './data-provider.service';
         GoogleSheetsService,
         ManualService,
         RapidApiService,
+        StockApiService,
         YahooFinanceService
       ],
       provide: 'DataProviderInterfaces',
@@ -67,6 +70,7 @@ import { DataProviderService } from './data-provider.service';
         googleSheetsService,
         manualService,
         rapidApiService,
+        stockApiService,
         yahooFinanceService
       ) => [
         alphaVantageService,
@@ -77,6 +81,7 @@ import { DataProviderService } from './data-provider.service';
         googleSheetsService,
         manualService,
         rapidApiService,
+        stockApiService,
         yahooFinanceService
       ]
     },
